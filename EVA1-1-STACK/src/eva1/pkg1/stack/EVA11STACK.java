efc670ca117501b5c4c9381af0dca4629713d17b
TheAvenger02 <emilioinformatica02@gmail.com>
2022-08-18 09:46:35 -0600

    Initial commit

---
+++ EVA1-1-STACK/src/eva1/pkg1/stack/EVA11STACK.java
@@ -0,0 +1,20 @@
+package eva1.pkg1.stack;
+/**
+ *
+ * @author emiliomurillo
+ */
+public class EVA11STACK {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+     int i = 1;
+     double d = 10.0;   
+    }
+}
---
+++ EVA1-2-STACKOVERFLOW/src/eva1/pkg2/stackoverflow/EVA12STACKOVERFLOW.java
@@ -0,0 +1,19 @@
+package eva1.pkg2.stackoverflow;
+
+/**
+ *
+ * @author emiliomurillo
+ */
+public class EVA12STACKOVERFLOW {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        A();
+    }
+}
