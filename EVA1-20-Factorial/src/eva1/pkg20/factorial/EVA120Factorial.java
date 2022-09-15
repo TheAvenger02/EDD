fc36a50ac5f491e429ed5ac00be43ee256fa0dfc
TheAvenger02 <emilioinformatica02@gmail.com>
2022-09-15 11:37:39 -0600

    Factorial

--- EVA1-20-Factorial/src/eva1/pkg20/factorial/EVA120Factorial.java
+++ EVA1-20-Factorial/src/eva1/pkg20/factorial/EVA120Factorial.java
@@ -1,5 +1,5 @@
 package eva1.pkg20.factorial;
-
+import java.util.Scanner;
 /**
  *
  * @author emiliomurillo
@@ -8,6 +8,19 @@ public class EVA120Factorial {
 
     public static void main(String[] args) {
         // factorial(5) -> 5 * 4 * 3 * 2 * 1 = 120
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        int num = sc.nextInt();
+        sc.close();
+         
+        System.out.println("\nEl factorial de " + num + " es: " + factorial(num));
     }
-    
+     
+    public static int factorial(int num){
+        if(num == 0){
+            return 1;
+        }
+        else
+            return num * factorial(num-1);
+    }
 }
