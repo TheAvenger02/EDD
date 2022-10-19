e141ea6ab8f8e655d943fcc380e056e89cb1bb0b
TheAvenger02 <emilioinformatica02@gmail.com>
2022-10-19 09:51:21 -0600

    Prácticas hasta el 19/10/2022

---
+++ EVA2/EVA2-5-STACK/src/eva2/pkg5/stack/EVA25STACK.java
@@ -0,0 +1,16 @@
+package eva2.pkg5.stack;
+
+/**
+ *
+ * @author emiliomurillo
+ */
+public class EVA25STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
